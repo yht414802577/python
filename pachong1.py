0a552ba2fd16f342f789fd63617928c8d6833366
yht414802577 <36285881+yht414802577@users.noreply.github.com>
2018-02-09 17:45:07 +0800

    添加注释

--- pachong1.py
+++ pachong1.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup
 
 req = urllib.request.urlopen('http://www.weather.com.cn/weather/101190401.shtml')
 html_data = req.read().decode('UTF-8')
-print(html_data)
+#print(html_data)
 
 bs = BeautifulSoup(html_data , 'html.parser')
 body = bs.body
-data = body.find('div',{'id':'7d'})
-input_bq = data.find('input')
-
-
+data = body.find('div',{'id':'7d'})  #查询div标签id = 7d
+input_bq = data.find('input')   #查询div标签id = 7d 下面的所有input标签
+#print(input_bq)
+ul = data.find('li')
+print(ul)
