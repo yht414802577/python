e73880625de540e7cfe674ef7882cd60268fb4ed
yht414802577 <36285881+yht414802577@users.noreply.github.com>
2018-02-28 15:04:54 +0800

    添加驱动下载备注

--- seleniumtest.py
+++ seleniumtest.py
@@ -1,7 +1,10 @@
 # coding = utf-8
+#   http://chromedriver.storage.googleapis.com/index.html    谷歌驱动
+#   https://github.com/mozilla/geckodriver/releases     火狐驱动
 
 from selenium import webdriver
 
+System.set
 browser = webdriver.Firefox()
 
 browser.get("http://www.baidu.com")
