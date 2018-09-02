eda6ad5e20bb8dc805f1a0796a048ac679c4362b
df <414802577@qq.com>
2018-09-02 23:46:41 +0800

    添加新文件

---
+++ process进程/child.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+"""
+@version:??
+@author:df
+"""
+
+import os, sys
+print('Hello from child', os.getpid(), sys.argv[1])
---
+++ process进程/multi5.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+"""
+@version:??
+@author:df
+"""
+
+import os
+from multiprocessing import Process
+
+def runprogram(arg):
+    os.execlp('python', 'python', 'child.py', str(arg))
+
+if __name__ == '__main__':
+    for i in range(5):
+        Process(target=runprogram,args=(i,)).start()
+    print('parent exit')
