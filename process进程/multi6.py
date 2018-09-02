e2b711c41fdebc5b47cfb4ddc7b8f6e70eba79f6
df <414802577@qq.com>
2018-09-02 23:52:48 +0800

    添加新文件

--- process进程/multi6.py
+++ process进程/multi6.py
@@ -12,7 +12,7 @@ def powers(x):
     return 2 ** x
 
 if __name__ == '__main__':
-    workers = Pool(processes=5)
+    workers = Pool(processes=4)
     results = workers.map(powers, [2]*100)
     print(results[:16])
     print(results[-2:])
