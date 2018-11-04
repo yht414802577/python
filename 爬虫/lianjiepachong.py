d24e839f69b883f546a9bdfb69a0e1042de7df42
df <414802577@qq.com>
2018-11-04 20:39:24 +0800

    爬虫

--- 爬虫/lianjiepachong.py
+++ 爬虫/lianjiepachong.py
@@ -3,3 +3,21 @@
 @version:??
 @author:df
 """
+
+from urllib import request
+from urllib.error import HTTPError, URLError
+import re
+import itertools
+
+def link_crawler(sead_url, link_regex):
+    craw_queue = [sead_url]
+    while craw_queue:
+        url = craw_queue.pop()
+        html = request.urlopen(url).read().decode()
+        for link in get_links(html):
+            if re.match(link_regex, link):
+                craw_queue.append(link)
+
+def get_links(html):
+    webpage_regex = re.compile('<a[~>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    return webpage_regex.findall(html)
