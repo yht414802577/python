f4ac40a5dcc5ce8c7130a5e1dccda0e0c8437994
df <414802577@qq.com>
2018-12-06 23:55:37 +0800

    上传

---
+++ PP4E/Gui/Intro/gui1b.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+"""
+@version:??
+@author:df
+"""
+
+from tkinter import *
+
+root = Tk()
+Label(root, text='Hello GUI world!').pack(side=TOP)
+root.mainloop()
---
+++ PP4E/Gui/Intro/gui1d.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+"""
+@version:??
+@author:df
+"""
+
+from tkinter import *
+
+Label(text='Hello GUI world!').pack()
+mainloop()
