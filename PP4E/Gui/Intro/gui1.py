57c021be37fd3d5c8f6d420723ae2803b0ddf413
df <414802577@qq.com>
2018-12-06 23:27:42 +0800

    上传

---
+++ PP4E/Gui/Intro/gui1.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+"""
+@version:??
+@author:df
+"""
+
+from tkinter import Label
+
+widget = Label(None, text='Hello GUI world!')
+widget.pack()
+widget.mainloop()
---
+++ PP4E/__init__.py
@@ -0,0 +1,5 @@
+# encoding: utf-8
+"""
+@version:??
+@author:df
+"""
