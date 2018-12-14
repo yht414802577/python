63ad0b74eba605475d24c43e507de245005c2573
df <414802577@qq.com>
2018-12-14 13:49:37 +0800

    音频

--- os模块/音频播放.py
+++ os模块/音频播放.py
@@ -8,6 +8,7 @@ import pyttsx3
 import pyaudio
 import json
 import requests
+import base64
 
 def get_audio():
     pa = pyaudio.PyAudio()
@@ -35,18 +36,41 @@ def get_token():
     client_id = 'xq4LTn8xQTKml4vnmY4NNtpG'
     client_secret = 'B9KcshnGDPjTjqyWgWyScdMmnm8zAQD7'
     host = url % (client_id, client_secret)
-    res = requests.get(host).text.decode()
+    res = requests.get(host).text
+    res = json.loads(res)
+    token = res['access_token']
+    return token
 
 
 
-def baidu_anlay(get_audio):
+def baidu_anlay():
+    token = get_token()
+    audio_data = get_audio()
+    audio_data_len = len(audio_data)
+    audio_data_bases64 = base64.b64encode(audio_data).decode()
+
     data = {
-        'format':'wav,'
+        'format':'wav',
         'rate':16000,
         'channel':1,
         'cuid':'asdfasdfasf',
-        'token':
+        'token':token,
+        'speech':audio_data_bases64,
+        'len':audio_data_len,
     }
+
+    data = json.dumps(data).encode()
+
+    url = 'http://vop.baidu.com/server_api'
+
+    header = {'Content-Type':'application/json',}
+
+    req = requests.post(url, headers=header, data=data, verify=False).text
+
+#{"corpus_no":"6634718094253233766","err_msg":"success.","err_no":0,"result":["嗯。"],"sn":"368266649571544765684"}
+    res = json.loads(req)['result'][0].replace('。','')
+
+    return res
 
 
 
@@ -56,5 +80,5 @@ def test():
     eg.runAndWait()
 
 if __name__ == '__main__':
-    test()
-    print(get_audio())
+
+    print(baidu_anlay())
