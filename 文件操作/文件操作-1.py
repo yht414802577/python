3fd5361e57421f6790afc4e59173358731b03a8b
df <414802577@qq.com>
2019-05-24 10:57:52 +0800

    游戏文件操作

--- 文件操作/文件操作-1.py
+++ 文件操作/文件操作-1.py
@@ -4,3 +4,14 @@
 @author:df
 """
 
+import pygame
+
+back_greoung_image_filename = 'xunguang-4.jpg'
+mouse_image_fuilename = '2014012906353538.jpg'
+
+
+pygame.init()
+screen = pygame.display.set_mode((640,480),0,32)
+
+
+pygame.quit()
